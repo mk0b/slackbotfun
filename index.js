33aa03a3fac5518def5ce37ed08989e3a1ee9d23
Megan <megankob@gmail.com>
2022-01-24 14:07:28 -0800

    Removed slackbots
    New bot types that are created do not play well with slackbots.

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-const SlackBot = require('slackbots');
 const axios = require('axios');
 
-//initializing slackbot
-const bot = new SlackBot({
-    token: 'xoxb-785032392182-2984680086503-NNF4WXXzyhIPvELAui67Np9N',
-    name: 'Animal Facts'
-});
+console.log('App Started');
 
-// Start handler - what happens when we start the bot.
-bot.on('start', () => {
-    var params = {
-        icon_imoji: ':horse:'
-    };
-
-    //define channel where bot exists.
-    bot.postMessageToChannel('general', 'Get ready to know a lot about animals...', params);
-});
+//removed everything as this was a classic slackbot set up with slackbots
